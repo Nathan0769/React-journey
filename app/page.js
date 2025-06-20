bcd45212be70c1945006b9098b503101073c71f4
Nathan0769 <nathanrivollier69@gmail.com>
2025-06-20 19:36:26 +0200

    Fix: deploy vercel and add Suspense

--- app/page.js
+++ app/page.js
@@ -2,15 +2,18 @@
 import { CardList } from "@/src/Card";
 import Header from "@/src/Header";
 import Menu from "@/src/Menu";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
-    <main className="min-h-screen p-8 m-auto flex h-full max-w-4xl flex-col px-4">
-      <Header className="max-w-4xl mx-auto" />
-      <div className="mb-4 mt-8 flex flex-1 gap-4 overflow-auto max-lg:flex-col">
-        <Menu />
-        <CardList></CardList>
-      </div>
-    </main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <main className="min-h-screen p-8 m-auto flex h-full max-w-4xl flex-col px-4">
+        <Header className="max-w-4xl mx-auto" />
+        <div className="mb-4 mt-8 flex flex-1 gap-4 overflow-auto max-lg:flex-col">
+          <Menu />
+          <CardList></CardList>
+        </div>
+      </main>
+    </Suspense>
   );
 }
